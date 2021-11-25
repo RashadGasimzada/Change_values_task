abdb1a22c4c1ee993f132d16400f00b3e6fcdcfe
Rashad Gasimzada <rashad.gasimzade.2000@gmail.com>
2021-11-25 19:32:04 +0400

    value change algorithm added

---
+++ Change_values_task/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Change_values_task
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int firstValue = 15;
+            int secondValue = 25;
+
+            firstValue = firstValue + secondValue;
+            secondValue = firstValue + secondValue;
+            firstValue = secondValue - firstValue;
+            secondValue = secondValue - firstValue * 2;
+
+            Console.WriteLine(firstValue + " " + secondValue);
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ Readme.md
@@ -0,0 +1,3 @@
+Change values in two variables algorithm
+
+Budur c# kodu və eyni prinsiplə işləyən .exe faylında yazılmış kodum
